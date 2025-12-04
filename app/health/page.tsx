d13fca9e6dabb68bbeada811d882b3a3ea0d26f7
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-04 22:41:20 +0600

    health debug added

---
+++ app/health/page.tsx
@@ -0,0 +1,12 @@
+import Health from "@/components/health/Health";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Health></Health>
+    </div>
+  );
+}
+
+export default page;
---
+++ components/health/Health.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+function Health() {
+  useEffect(() => {
+    const checkHealth = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/health`);
+        const data = await res.json();
+        console.log("res", data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    checkHealth();
+  }, []);
+  return <div className="min-h-screen bg-white">Hello world</div>;
+}
+
+export default Health;
